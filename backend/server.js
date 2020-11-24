2f76957aca6a4669c6a9e2da01f2e45fd7e70981
Wesley <wesaboo@gmail.com>
2020-11-23 19:33:19 -0800

    put axios call in button

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const axios = require("axios");
 const app = express();
 const port = 5000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -13,7 +18,7 @@ app.get("/getSummonerName", (req, res) => {
     const data = await axios.get(
       "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/mistahpig?api_key=RGAPI-09ea864d-e4b8-41df-9a41-dc97b02fe32c"
     );
-    res.send(data.data);
+    res.json(data.data);
   }
   getSummoner();
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,23 @@
-import React, { useState } from 'react'
-import './App.css'
-import axios from 'axios'
-;(function () {
-  var cors_api_host = 'cors-anywhere.herokuapp.com'
-  var cors_api_url = 'https://' + cors_api_host + '/'
-  var slice = [].slice
-  var origin = window.location.protocol + '//' + window.location.host
-  var open = XMLHttpRequest.prototype.open
-  XMLHttpRequest.prototype.open = function () {
-    var args = slice.call(arguments)
-    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1])
-    if (
-      targetOrigin &&
-      targetOrigin[0].toLowerCase() !== origin &&
-      targetOrigin[1] !== cors_api_host
-    ) {
-      args[1] = cors_api_url + args[1]
-    }
-    return open.apply(this, args)
-  }
-})()
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [summonerInfo, setSummonerInfo] = useState({})
+  const [summonerInfo, setSummonerInfo] = useState({});
 
-  const getSummonerName = async () => {
-    const res = await axios.get(
-      'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/mistahpig?api_key=RGAPI-7fde24cf-5990-45f9-81b9-d10de3363462'
-    )
-    setSummonerInfo(res.data)
-  }
-
-  getSummonerName()
+  const clickHandler = () => {
+    axios.get("http://localhost:5000/getSummonerName").then((res) => {
+      setSummonerInfo(res.data);
+    });
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       Hello World
       <pre>{JSON.stringify(summonerInfo, null, 2)}</pre>
+      <button onClick={clickHandler}>Click</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
