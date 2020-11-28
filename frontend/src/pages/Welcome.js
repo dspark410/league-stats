5a2712199dd355b177894de907a6a0723dab9a97
Wesley <wesaboo@gmail.com>
2020-11-28 04:01:55 -0800

    change sessionStorage to make axios call and set response directly to mastery

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,60 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import style from './welcome.module.css'
-import axios from 'axios'
-import MasteryCard from '../components/MasteryCard'
+import React, { useState, useEffect } from "react";
+import style from "./welcome.module.css";
+import axios from "axios";
+import MasteryCard from "../components/MasteryCard";
 
 function Welcome({ summonerInfo, champInfo }) {
-  const [mastery, setMastery] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [filteredChamps, setFilteredChamps] = useState([])
-  const [session, setSession] = useState({})
+  const [mastery, setMastery] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [filteredChamps, setFilteredChamps] = useState([]);
 
   useEffect(() => {
     if (!summonerInfo.id) {
-      console.log('Summoner info not in state')
+      console.log("Summoner info not in state");
+      //Get Sessions data
+      const sessionData = JSON.parse(sessionStorage.getItem("summonerInfo"));
+
+      axios
+        .get(`http://localhost:5000/masteries/${sessionData.id}`)
+        .then((res) => {
+          setMastery(res.data);
+          setLoading(false);
+          console.log(res.data);
+        });
     } else {
       axios
-        .get(`http://localhost:5000/masteries/${summonerInfo.id || session.id}`)
+        .get(`http://localhost:5000/masteries/${summonerInfo.id}`)
         .then((res) => {
-          setMastery(res.data)
-          setLoading(false)
-          console.log(res.data)
-        })
+          setMastery(res.data);
+          setLoading(false);
+          console.log(res.data);
+        });
     }
-  }, [summonerInfo.id])
+  }, [summonerInfo.id]);
 
   useEffect(() => {
-    const championInfoArray = []
+    const championInfoArray = [];
     mastery.forEach((champ) => {
-      championInfoArray.push(champ.championId)
-    })
+      championInfoArray.push(champ.championId);
+    });
 
     const masteryChamp = champInfo.filter((champ) => {
       if (championInfoArray.indexOf(Number(champ.key)) >= 0) {
-        return true
+        return true;
       } else {
-        return false
+        return false;
       }
-    })
-    setFilteredChamps(masteryChamp)
-    //Get Sessions data
-    const sessionData = JSON.parse(sessionStorage.getItem('summonerInfo'))
-    setSession(sessionData)
-  }, [mastery])
+    });
+    setFilteredChamps(masteryChamp);
+  }, [mastery]);
 
   return (
     <div className={style.welcomeBackgroundContainer}>
       <div className={style.welcomeContainer}>
         <div>
-          <h1>Welcome {summonerInfo.name || session.name}</h1>
+          <h1>Welcome {summonerInfo.name}</h1>
         </div>
-        <div>{loading ? '' : mastery[0].championPoints}</div>
-        <div>{loading ? '' : mastery[1].championPoints}</div>
-        <div>{loading ? '' : mastery[2].championPoints}</div>
+        <div>{loading ? "" : mastery[0].championPoints}</div>
+        <div>{loading ? "" : mastery[1].championPoints}</div>
+        <div>{loading ? "" : mastery[2].championPoints}</div>
         <pre>{JSON.stringify(filteredChamps, null, 2)}</pre>
       </div>
     </div>
-  )
+  );
 }
 
-export default Welcome
+export default Welcome;
