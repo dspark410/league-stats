5abad7aaf7020ab5a717bd312165075fbdb2c6d5
mistahpig <dspark410@gmail.com>
2020-11-29 13:23:00 -0800

    adds styling for masterycard

--- frontend/src/components/MasteryCard.js
+++ frontend/src/components/MasteryCard.js
@@ -4,7 +4,7 @@ import style from './masterycard.module.css'
 function MasteryCard({ filteredChamps, loading }) {
   return (
     <div className={style.masteryCardContainer}>
-      <div>
+      <div className={style.masteryCard}>
         {loading ? (
           ''
         ) : (
@@ -13,11 +13,17 @@ function MasteryCard({ filteredChamps, loading }) {
             src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${filteredChamps[0].image}_0.jpg`}
           />
         )}
-        <div>{loading ? '' : filteredChamps[0].name}</div>
-        <div>{loading ? '' : filteredChamps[0].points}</div>
-        <div> {loading ? '' : filteredChamps[0].level}</div>
+        <div className={style.name}>
+          {loading ? '' : filteredChamps[0].name}
+        </div>
+        <div className={style.points}>
+          {loading ? '' : filteredChamps[0].points}
+        </div>
+        <div className={style.level}>
+          {loading ? '' : filteredChamps[0].level}
+        </div>
       </div>
-      <div>
+      <div className={style.masteryCard}>
         {loading ? (
           ''
         ) : (
@@ -26,11 +32,18 @@ function MasteryCard({ filteredChamps, loading }) {
             src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${filteredChamps[1].image}_0.jpg`}
           />
         )}
-        <div>{loading ? '' : filteredChamps[1].name}</div>
-        <div>{loading ? '' : filteredChamps[1].points}</div>
-        <div> {loading ? '' : filteredChamps[1].level}</div>
+        <div className={style.name}>
+          {loading ? '' : filteredChamps[1].name}
+        </div>
+        <div className={style.points}>
+          {loading ? '' : filteredChamps[1].points}
+        </div>
+        <div className={style.level}>
+          {' '}
+          {loading ? '' : filteredChamps[1].level}
+        </div>
       </div>
-      <div>
+      <div className={style.masteryCard}>
         {loading ? (
           ''
         ) : (
@@ -39,9 +52,16 @@ function MasteryCard({ filteredChamps, loading }) {
             src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${filteredChamps[2].image}_0.jpg`}
           />
         )}
-        <div>{loading ? '' : filteredChamps[2].name}</div>
-        <div>{loading ? '' : filteredChamps[2].points}</div>
-        <div> {loading ? '' : filteredChamps[2].level}</div>
+        <div className={style.name}>
+          {loading ? '' : filteredChamps[2].name}
+        </div>
+        <div className={style.points}>
+          {loading ? '' : filteredChamps[2].points}
+        </div>
+        <div className={style.level}>
+          {' '}
+          {loading ? '' : filteredChamps[2].level}
+        </div>
       </div>
     </div>
   )
