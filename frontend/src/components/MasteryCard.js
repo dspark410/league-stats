278d7bd44814e8e46a53d91007e0ad56e49cc526
mistahpig <dspark410@gmail.com>
2020-11-30 12:34:20 -0800

    adds mastery card styling, images, and icons

--- frontend/src/components/MasteryCard.js
+++ frontend/src/components/MasteryCard.js
@@ -9,15 +9,200 @@ function MasteryCard({ filteredChamps, loading }) {
           ''
         ) : (
           <img
+            className={style.championImage}
+            alt={filteredChamps[0].image}
+            src={`http://ddragon.leagueoflegends.com/cdn/10.24.1/img/champion/${filteredChamps[0].image}.png`}
+          />
+        )}
+        {loading ? (
+          ''
+        ) : (
+          <img
+            alt={'mastery level border'}
+            src={
+              filteredChamps[0].level === 7
+                ? process.env.PUBLIC_URL + '/images/level7.png'
+                : filteredChamps[0].level === 6
+                ? process.env.PUBLIC_URL + '/images/level6.png'
+                : filteredChamps[0].level === 5
+                ? process.env.PUBLIC_URL + '/images/level5.png'
+                : filteredChamps[0].level === 4
+                ? process.env.PUBLIC_URL + '/images/level4.png'
+                : filteredChamps[0].level === 3
+                ? process.env.PUBLIC_URL + '/images/level3.png'
+                : filteredChamps[0].level === 2
+                ? process.env.PUBLIC_URL + '/images/level2.png'
+                : filteredChamps[0].level === 1
+                ? process.env.PUBLIC_URL + '/images/level1.png'
+                : process.env.PUBLIC_URL + '/images/level0.png'
+            }
+          />
+        )}
+
+        <div className={style.name}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/champion.png'}
+            alt='champion icon'
+          />
+          <span>{loading ? '' : filteredChamps[0].name}</span>
+        </div>
+        <div className={style.points}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/mastery.png'}
+            alt='mastery icon'
+          />
+          <span>
+            {loading ? '' : filteredChamps[0].points.toLocaleString('en')} pts
+          </span>
+        </div>
+        <div className={style.level}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/level.png'}
+            alt='mastery icon'
+          />
+          <span>mastery lvl {loading ? '' : filteredChamps[0].level}</span>
+        </div>
+      </div>
+      <div className={style.masteryCard}>
+        {loading ? (
+          ''
+        ) : (
+          <img
+            className={style.championImage}
+            alt={filteredChamps[1].image}
+            src={`http://ddragon.leagueoflegends.com/cdn/10.24.1/img/champion/${filteredChamps[1].image}.png`}
+          />
+        )}
+        {loading ? (
+          ''
+        ) : (
+          <img
+            alt={'mastery level border'}
+            src={
+              filteredChamps[1].level === 7
+                ? process.env.PUBLIC_URL + '/images/level7.png'
+                : filteredChamps[1].level === 6
+                ? process.env.PUBLIC_URL + '/images/level6.png'
+                : filteredChamps[1].level === 5
+                ? process.env.PUBLIC_URL + '/images/level5.png'
+                : filteredChamps[1].level === 4
+                ? process.env.PUBLIC_URL + '/images/level4.png'
+                : filteredChamps[1].level === 3
+                ? process.env.PUBLIC_URL + '/images/level3.png'
+                : filteredChamps[1].level === 2
+                ? process.env.PUBLIC_URL + '/images/level2.png'
+                : filteredChamps[1].level === 1
+                ? process.env.PUBLIC_URL + '/images/level1.png'
+                : process.env.PUBLIC_URL + '/images/level0.png'
+            }
+          />
+        )}
+        <div className={style.name}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/champion.png'}
+            alt='champion icon'
+          />
+          <span>{loading ? '' : filteredChamps[1].name}</span>
+        </div>
+        <div className={style.points}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/mastery.png'}
+            alt='mastery icon'
+          />
+          <span>
+            {loading ? '' : filteredChamps[1].points.toLocaleString('en')} pts
+          </span>
+        </div>
+        <div className={style.level}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/level.png'}
+            alt='mastery icon'
+          />
+          <span>mastery lvl {loading ? '' : filteredChamps[1].level}</span>
+        </div>
+      </div>
+      <div className={style.masteryCard}>
+        {loading ? (
+          ''
+        ) : (
+          <img
+            className={style.championImage}
+            alt={filteredChamps[2].image}
+            src={`http://ddragon.leagueoflegends.com/cdn/10.24.1/img/champion/${filteredChamps[2].image}.png`}
+          />
+        )}
+        {loading ? (
+          ''
+        ) : (
+          <img
+            alt={'mastery level border'}
+            src={
+              filteredChamps[2].level === 7
+                ? process.env.PUBLIC_URL + '/images/level7.png'
+                : filteredChamps[2].level === 6
+                ? process.env.PUBLIC_URL + '/images/level6.png'
+                : filteredChamps[2].level === 5
+                ? process.env.PUBLIC_URL + '/images/level5.png'
+                : filteredChamps[2].level === 4
+                ? process.env.PUBLIC_URL + '/images/level4.png'
+                : filteredChamps[2].level === 3
+                ? process.env.PUBLIC_URL + '/images/level3.png'
+                : filteredChamps[2].level === 2
+                ? process.env.PUBLIC_URL + '/images/level2.png'
+                : filteredChamps[2].level === 1
+                ? process.env.PUBLIC_URL + '/images/level1.png'
+                : process.env.PUBLIC_URL + '/images/level0.png'
+            }
+          />
+        )}
+        <div className={style.name}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/champion.png'}
+            alt='champion icon'
+          />
+          <span>{loading ? '' : filteredChamps[2].name}</span>
+        </div>
+        <div className={style.points}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/mastery.png'}
+            alt='mastery icon'
+          />
+          <span>
+            {loading ? '' : filteredChamps[2].points.toLocaleString('en')} pts
+          </span>
+        </div>
+        <div className={style.level}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/level.png'}
+            alt='mastery icon'
+          />
+          <span>mastery lvl {loading ? '' : filteredChamps[2].level}</span>
+        </div>
+      </div>
+      {/* <div className={style.masteryCard}>
+        {loading ? (
+          ''
+        ) : (
+          <img
             alt={filteredChamps[0].image}
             src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${filteredChamps[0].image}_0.jpg`}
           />
         )}
         <div className={style.name}>
-          {loading ? '' : filteredChamps[0].name}
+          <img
+            src={process.env.PUBLIC_URL + '/images/champion.png'}
+            alt='champion icon'
+          />
+          <span>{loading ? '' : filteredChamps[0].name}</span>
         </div>
         <div className={style.points}>
-          {loading ? '' : filteredChamps[0].points}
+          <img
+            src={process.env.PUBLIC_URL + '/images/mastery.png'}
+            alt='mastery icon'
+          />
+          <span>
+            {loading ? '' : filteredChamps[0].points.toLocaleString('en')} pts
+          </span>
         </div>
         <div className={style.level}>
           {loading ? '' : filteredChamps[0].level}
@@ -33,10 +218,20 @@ function MasteryCard({ filteredChamps, loading }) {
           />
         )}
         <div className={style.name}>
-          {loading ? '' : filteredChamps[1].name}
+          <img
+            src={process.env.PUBLIC_URL + '/images/champion.png'}
+            alt='champion icon'
+          />
+          <span>{loading ? '' : filteredChamps[1].name}</span>
         </div>
         <div className={style.points}>
-          {loading ? '' : filteredChamps[1].points}
+          <img
+            src={process.env.PUBLIC_URL + '/images/mastery.png'}
+            alt='mastery icon'
+          />
+          <span>
+            {loading ? '' : filteredChamps[1].points.toLocaleString('en')} pts
+          </span>
         </div>
         <div className={style.level}>
           {' '}
@@ -53,16 +248,26 @@ function MasteryCard({ filteredChamps, loading }) {
           />
         )}
         <div className={style.name}>
-          {loading ? '' : filteredChamps[2].name}
+          <img
+            src={process.env.PUBLIC_URL + '/images/champion.png'}
+            alt='champion icon'
+          />
+          <span>{loading ? '' : filteredChamps[2].name}</span>
         </div>
         <div className={style.points}>
-          {loading ? '' : filteredChamps[2].points}
+          <img
+            src={process.env.PUBLIC_URL + '/images/mastery.png'}
+            alt='mastery icon'
+          />
+          <span>
+            {loading ? '' : filteredChamps[2].points.toLocaleString('en')} pts
+          </span>
         </div>
         <div className={style.level}>
           {' '}
           {loading ? '' : filteredChamps[2].level}
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
