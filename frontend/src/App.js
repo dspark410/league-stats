f132859178d6abb131197e77506add78e3f16549
mistahpig <dspark410@gmail.com>
2020-11-29 19:17:09 -0800

    adds optimusprinceps font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,90 +1,90 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
-import Home from "./pages/Home";
-import Welcome from "./pages/Welcome";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+import Home from './pages/Home'
+import Welcome from './pages/Welcome'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from "react-router-dom";
+} from 'react-router-dom'
 
 function App() {
-  const [summonerInfo, setSummonerInfo] = useState({});
-  const [inputValue, setInputValue] = useState("");
-  const [redirect, setRedirect] = useState(false);
-  const [champions, setChampions] = useState([]);
-  const [champInfo, setChampInfo] = useState([]);
+  const [summonerInfo, setSummonerInfo] = useState({})
+  const [inputValue, setInputValue] = useState('')
+  const [redirect, setRedirect] = useState(false)
+  const [champions, setChampions] = useState([])
+  const [champInfo, setChampInfo] = useState([])
 
   useEffect(() => {
     axios
       .get(
-        "http://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json"
+        'http://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json'
       )
       .then((res) => {
-        setChampions(res.data.data);
-      });
-  }, []);
+        setChampions(res.data.data)
+      })
+  }, [])
 
   useEffect(() => {
-    const champNameArray = Object.keys(champions);
-    const championArray = Object.values(champions);
+    const champNameArray = Object.keys(champions)
+    const championArray = Object.values(champions)
 
-    const newArray = [];
+    const newArray = []
 
     for (let i = 0; i < champNameArray.length; i++) {
-      const name = championArray[i].name;
-      const key = championArray[i].key;
-      const image = championArray[i].image.full.split(".")[0];
+      const name = championArray[i].name
+      const key = championArray[i].key
+      const image = championArray[i].image.full.split('.')[0]
 
       const object = {
         name,
         key,
         image,
-      };
+      }
 
-      newArray.push(object);
+      newArray.push(object)
     }
-    setChampInfo(newArray);
-  }, [champions]);
+    setChampInfo(newArray)
+  }, [champions])
 
   const handleOnChange = (e) => {
-    setInputValue(e.target.value);
-  };
+    setInputValue(e.target.value)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    if (inputValue.trim() === "") {
-      console.log("Please enter something");
+    if (inputValue.trim() === '') {
+      console.log('Please enter something')
     } else {
       axios
         .get(`http://localhost:5000/getSummonerName/${inputValue}`)
         .then((res) => {
           if (res.status) {
-            console.log(res.data);
+            console.log(res.data)
           } else {
-            setSummonerInfo(res.data);
+            setSummonerInfo(res.data)
 
             //Set session data
-            sessionStorage.setItem("summonerInfo", JSON.stringify(res.data));
-            setRedirect(true);
+            sessionStorage.setItem('summonerInfo', JSON.stringify(res.data))
+            setRedirect(true)
           }
-        });
+        })
     }
-  };
+  }
 
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Switch>
           <Route
             exact
-            path="/"
+            path='/'
             render={() =>
               redirect ? (
-                <Redirect to="/welcome" />
+                <Redirect to='/welcome' />
               ) : (
                 <Home
                   summonerInfo={summonerInfo}
@@ -98,7 +98,7 @@ function App() {
             }
           />
           <Route
-            path="/welcome"
+            path='/welcome'
             render={(props) => (
               <Welcome
                 {...props}
@@ -111,7 +111,7 @@ function App() {
         </Switch>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
