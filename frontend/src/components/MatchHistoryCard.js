d269e08f7af0a57c5f2b226cb3ebfdd18f0353ac
Wesley <wesaboo@gmail.com>
2020-12-03 17:07:57 -0800

    change the way object is created, return as a single object instead of obj + int

--- frontend/src/components/MatchHistoryCard.js
+++ frontend/src/components/MatchHistoryCard.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import style from './matchhistory.module.css'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import style from "./matchhistory.module.css";
+import axios from "axios";
 
 // MATCH TIMESLINES API
 // https://na1.api.riotgames.com/lol/match/v4/timelines/by-match/3630397822?api_key=RGAPI-f3372fe9-4a88-4d2f-917b-54974292c5f6
@@ -9,10 +9,10 @@ function MatchHistoryCard({ matchDetails, summonerInfo }) {
   // const [types, setTypes] = useState([])
   // const [modes, setModes] = useState([])
   //const [maps, setMaps] = useState([])
-  const [queues, setQueues] = useState([])
-  const [gameDetails, setGameDetails] = useState([])
-  const [participant, setParticipant] = useState([])
-  const [summoner, setSummoner] = useState([])
+  const [queues, setQueues] = useState([]);
+  const [gameDetails, setGameDetails] = useState([]);
+  //const [participant, setParticipant] = useState([]);
+  //const [summoner, setSummoner] = useState([]);
   useEffect(() => {
     // axios
     //   .get('http://static.developer.riotgames.com/docs/lol/gameTypes.json')
@@ -21,48 +21,58 @@ function MatchHistoryCard({ matchDetails, summonerInfo }) {
     //   .get('http://static.developer.riotgames.com/docs/lol/gameModes.json')
     //   .then((res) => setModes(res.data))
     axios
-      .get('http://localhost:5000/queueType')
-      .then((res) => setQueues(res.data))
+      .get("http://localhost:5000/queueType")
+      .then((res) => setQueues(res.data));
     // axios.get('http://localhost:5000/mapList').then((res) => setMaps(res.data))
-  }, [])
+  }, []);
 
-  const sessionData = JSON.parse(sessionStorage.getItem('summonerInfo'))
+  const sessionData = JSON.parse(sessionStorage.getItem("summonerInfo"));
 
   useEffect(() => {
-    const gameDetailsArr = []
-
+    const gameDetailsArr = [];
     matchDetails.forEach((match) => {
+      let matchObj;
+      let participantObj;
       queues.forEach((queue) => {
         if (match.queueId === queue.queueId) {
-          const obj = {
+          matchObj = {
             map: queue.map,
             gameType: queue.description,
-          }
-          gameDetailsArr.push(obj)
-          setGameDetails(gameDetailsArr)
+          };
         }
-      })
-    })
-  }, [matchDetails])
-
-  useEffect(() => {
-    const participantArr = []
-
-    matchDetails.forEach((match) => {
+      });
       match.participantIdentities.forEach((id) => {
         if (
           id.player.accountId === summonerInfo.accountId ||
           id.player.accountId === sessionData.accountId
         ) {
-          const obj = {
-            participantId: id.participantId,
-          }
-          participantArr.push(obj)
-          setParticipant(participantArr)
+          participantObj = id.participantId;
+          matchObj.participantId = participantObj;
+          gameDetailsArr.push(matchObj);
         }
-      })
-    })
-  }, [matchDetails])
+      });
+    });
+    setGameDetails(gameDetailsArr);
+  }, [matchDetails]);
+
+  // useEffect(() => {
+  //   const participantArr = []
+
+  //   matchDetails.forEach((match) => {
+  //     match.participantIdentities.forEach((id) => {
+  //       if (
+  //         id.player.accountId === summonerInfo.accountId ||
+  //         id.player.accountId === sessionData.accountId
+  //       ) {
+  //         const obj = {
+  //           participantId: id.participantId,
+  //         }
+  //         participantArr.push(obj)
+  //         setParticipant(participantArr)
+  //       }
+  //     })
+  //   })
+  // }, [matchDetails])
 
   // useEffect(() => {
   //   const summonerArr = []
@@ -84,9 +94,8 @@ function MatchHistoryCard({ matchDetails, summonerInfo }) {
   return (
     <div className={style.matchContainer}>
       <pre>{JSON.stringify(gameDetails, null, 2)} </pre>
-      <pre>{JSON.stringify(participant, null, 2)} </pre>
     </div>
-  )
+  );
 }
 
-export default MatchHistoryCard
+export default MatchHistoryCard;
