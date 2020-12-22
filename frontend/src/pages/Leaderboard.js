6ea241b6b0869883d265c3a0281d4360ebdce36e
mistahpig <dspark410@gmail.com>
2020-12-22 12:53:10 -0800

    adds table for leaderboard

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -5,19 +5,54 @@ import style from './leaderboard.module.css'
 function Leaderboard() {
   const [solo, setSolo] = useState([])
   const [flex, setFlex] = useState([])
+  const [soloTier, setSoloTier] = useState([])
+  const [flexTier, setFlexTier] = useState([])
 
   const url = process.env.REACT_APP_API_URL || ''
 
   useEffect(() => {
-    axios
-      .get(`${url}/leaderboard/solo`)
-      .then((res) => setSolo(res.data.entries))
-    axios
-      .get(`${url}/leaderboard/flex`)
-      .then((res) => setFlex(res.data.entries))
+    axios.get(`${url}/leaderboard/solo`).then((res) => {
+      setSolo(res.data.entries)
+      setSoloTier(res.data.tier)
+    })
+    axios.get(`${url}/leaderboard/flex`).then((res) => {
+      setFlex(res.data.entries)
+      setFlexTier(res.data.tier)
+    })
   }, [])
 
-  return <div></div>
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Ranked Solo</th>
+            <th>Ranked Flex</th>
+          </tr>
+          {solo
+            .sort((a, b) => b.leaguePoints - a.leaguePoints)
+            .map((summoner, i) => (
+              <tr key={i}>
+                <td>{i + 1}</td>
+                <td>
+                  <div>{summoner.summonerName}</div>
+                </td>
+                <td>
+                  <div>{soloTier}</div>
+                  <div>LP: {summoner.leaguePoints}</div>
+                  <div>
+                    <span>Wins: {summoner.wins}</span>
+                    <span>Losses: {summoner.losses}</span>
+                  </div>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Leaderboard
