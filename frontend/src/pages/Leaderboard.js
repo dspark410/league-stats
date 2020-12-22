e8a5fafc9d1b915d7441dce7b435f2905757a4f4
mistahpig <dspark410@gmail.com>
2020-12-21 19:08:06 -0800

    adds solo and flex challenger queries to server.js, makes calls from the front end and sets into state.

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import style from './leaderboard.module.css'
 
 function Leaderboard() {
-  return <div>THIS IS THE LEADERBOARD PAGE</div>
+  const [solo, setSolo] = useState([])
+  const [flex, setFlex] = useState([])
+
+  const url = process.env.REACT_APP_API_URL || ''
+
+  useEffect(() => {
+    axios
+      .get(`${url}/leaderboard/solo`)
+      .then((res) => setSolo(res.data.entries))
+    axios
+      .get(`${url}/leaderboard/flex`)
+      .then((res) => setFlex(res.data.entries))
+  }, [])
+
+  return <div></div>
 }
 
 export default Leaderboard
