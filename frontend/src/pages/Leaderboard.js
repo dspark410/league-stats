73e3876155018f51033ec29d8a88bf31d695f4d4
Wesley <wesaboo@gmail.com>
2020-12-27 15:34:48 -0800

    move sort to right after axios call to keep consistency, resize image in leaderboard

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -7,24 +7,22 @@ function Leaderboard({ version }) {
   // const [flex, setFlex] = useState([])
   const [soloTier, setSoloTier] = useState([]);
   // const [flexTier, setFlexTier] = useState([])
-  const [summonerName, setSummonerName] = useState([]);
-  const [loader, setLoader] = useState(true);
 
   const url = process.env.REACT_APP_API_URL || "";
 
   useEffect(() => {
     axios.get(`${url}/leaderboard/solo`).then((res) => {
-      const soloLeaderboard = res.data.entries.slice(0, 2);
+      const soloLeaderboard = res.data.entries
+        .sort((a, b) => b.leaguePoints - a.leaguePoints)
+        .slice(0, 2);
       soloLeaderboard.forEach(async (player) => {
         axios.get(`${url}/getSummonerId/${player.summonerId}`).then((res) => {
           player.icon = res.data.profileIconId;
         });
       });
       setSolo(soloLeaderboard);
-      //setSoloTier(res.data.tier)
+      setSoloTier(res.data.tier);
     });
-
-    setLoader(false);
 
     // axios.get(`${url}/leaderboard/flex`).then((res) => {
     //   setFlex(res.data.entries)
@@ -44,44 +42,38 @@ function Leaderboard({ version }) {
             Win Ratio
           </th>
         </tr>
-        {loader ? (
-          <tr />
-        ) : (
-          solo
-            .sort((a, b) => b.leaguePoints - a.leaguePoints)
-            .map((summoner, i) => (
-              <tr className={style.row} key={i}>
-                <td className={style.td}>{i + 1}</td>
-                <td className={style.td}>
-                  <img
-                    alt="profile icon"
-                    className={style.profileIcon}
-                    // Grab profile icon
-                    src={
-                      `https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${summoner.icon}.png` ||
-                      process.env.PUBLIC_URL + "/images/emptyitem.png"
-                    }
-                  />
-                  {summoner.summonerName}
-                </td>
-                <td className={style.td}>{soloTier}</td>
-                <td className={style.td}>{summoner.leaguePoints}</td>
-                <td className={`${style.tdWinRatio}`}>
-                  <div className={style.winRatio}>
-                    <span>{summoner.wins}/</span>
-                    <span>{summoner.losses}</span>
-                    <span>
-                      {(
-                        (summoner.wins / (summoner.wins + summoner.losses)) *
-                        100
-                      ).toFixed(0)}
-                      %
-                    </span>
-                  </div>
-                </td>
-              </tr>
-            ))
-        )}
+        {solo.map((summoner, i) => (
+          <tr className={style.row} key={i}>
+            <td className={style.td}>{i + 1}</td>
+            <td className={style.td}>
+              <img
+                alt="profile icon"
+                className={style.profileIcon}
+                // Grab profile icon
+                src={
+                  `https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${summoner.icon}.png` ||
+                  process.env.PUBLIC_URL + "/images/emptyitem.png"
+                }
+              />
+              {summoner.summonerName}
+            </td>
+            <td className={style.td}>{soloTier}</td>
+            <td className={style.td}>{summoner.leaguePoints}</td>
+            <td className={`${style.tdWinRatio}`}>
+              <div className={style.winRatio}>
+                <span>{summoner.wins}/</span>
+                <span>{summoner.losses}</span>
+                <span>
+                  {(
+                    (summoner.wins / (summoner.wins + summoner.losses)) *
+                    100
+                  ).toFixed(0)}
+                  %
+                </span>
+              </div>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
