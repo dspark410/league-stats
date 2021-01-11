b41eeb4a1d026b4aee2b83e65cd10114fabf204f
mistahpig <dspark410@gmail.com>
2021-01-11 12:12:03 -0800

    new home page

--- frontend/src/pages/ChampionDetail.js
+++ frontend/src/pages/ChampionDetail.js
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import style from "./championdetail.module.css";
-import Tooltip from "../components/Tooltip";
-import Loader from "../components/Loader";
+import React, { useState } from 'react'
+import style from './championdetail.module.css'
+import Tooltip from '../components/Tooltip'
+import Loader from '../components/Loader'
 
 export default function ChampionDetail({ version, champDetail, itemObj }) {
-  const [video, setVideo] = useState("Q");
-  const [loading, setLoading] = useState(false);
+  const [video, setVideo] = useState('Q')
+  const [loading, setLoading] = useState(false)
 
   const selectVideo = (e) => {
-    setLoading(true);
-    const key = e.target.getAttribute("value");
-    setVideo(key);
+    setLoading(true)
+    const key = e.target.getAttribute('value')
+    setVideo(key)
     setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  };
+      setLoading(false)
+    }, 2000)
+  }
 
-  let key;
+  let key
 
   if (champDetail.key.length === 1) {
-    key = "000" + champDetail.key;
+    key = '000' + champDetail.key
   } else if (champDetail.key.length === 2) {
-    key = "00" + champDetail.key;
+    key = '00' + champDetail.key
   } else if (champDetail.key.length === 3) {
-    key = "0" + champDetail.key;
+    key = '0' + champDetail.key
   } else {
-    key = champDetail.key;
+    key = champDetail.key
   }
 
   // const backgroundURL = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champDetail.id}_0.jpg`
@@ -74,7 +74,7 @@ export default function ChampionDetail({ version, champDetail, itemObj }) {
         <div className={style.buildContainer}>
           <h3>Recommended Build</h3>
           {champDetail.recommended.map((build, i) => {
-            return build.mode === "CLASSIC" ? (
+            return build.mode === 'CLASSIC' ? (
               <div className={style.buildHeader} key={i}>
                 {/* <h3>{build.type.split('-')[1]}</h3> */}
 
@@ -98,14 +98,14 @@ export default function ChampionDetail({ version, champDetail, itemObj }) {
                                 />
                               </Tooltip>
                             </div>
-                          );
+                          )
                         })}
                       </div>
                     </div>
-                  );
+                  )
                 })}
               </div>
-            ) : null;
+            ) : null
           })}
         </div>
 
@@ -120,20 +120,20 @@ export default function ChampionDetail({ version, champDetail, itemObj }) {
               >
                 <img
                   className={style.spellImage}
-                  alt="champion passive"
+                  alt='champion passive'
                   src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/passive/${champDetail.passive.image.full}`}
                 />
               </Tooltip>
             </div>
             {champDetail.spells.map((spell, i) => {
               const buttonKey =
-                i === 0 ? "Q" : i === 1 ? "W" : i === 2 ? "E" : "R";
+                i === 0 ? 'Q' : i === 1 ? 'W' : i === 2 ? 'E' : 'R'
               const tooltipInfo = `
                   <p>Spell Cooldown: ${spell.cooldownBurn} seconds</p>
                   <p>
                     Spell Cost: ${spell.costBurn} ${champDetail.partype}
                   </p>
-                `;
+                `
 
               return (
                 <div key={i} className={style.spellImageContainer}>
@@ -150,12 +150,12 @@ export default function ChampionDetail({ version, champDetail, itemObj }) {
                       className={`${style.spellImage} ${
                         video === buttonKey ? style.spellImageActive : null
                       }`}
-                      alt="champion skills"
+                      alt='champion skills'
                       src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${spell.image.full}`}
                     />
                   </Tooltip>
                 </div>
-              );
+              )
             })}
           </div>
           {loading ? (
@@ -173,5 +173,5 @@ export default function ChampionDetail({ version, champDetail, itemObj }) {
         </div>
       </div>
     </div>
-  ) : null;
+  ) : null
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,23 +1,36 @@
-import React from "react";
-import style from "./home.module.css";
-import { AiOutlineSearch } from "react-icons/ai";
+import React from 'react'
+import style from './home.module.css'
+import { AiOutlineSearch } from 'react-icons/ai'
+import background from '../components/images/brand.jpg'
 
 function Home({ change, submit, inputResponse }) {
   return (
-    <div className={style.homeBackgroundContainer}>
+    <div
+      className={style.homeBackgroundContainer}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className={style.overlay}></div>
+      <div className={style.footer}>
+        LeagueStats.com isn't endorsed by Riot Games and doesn't reflect the
+        views or opinions of Riot Games or anyone officially involved in
+        producing or managing Riot Games properties. Riot Games, and all
+        associated properties are trademarks or registered trademarks of Riot
+        Games, Inc.
+      </div>
+
       <div className={style.homeContainer}>
-        <img
+        {/* <img
           className={style.homeImage}
           alt="teemo"
           src={process.env.PUBLIC_URL + `/images/teemo.png`}
-        />
+        /> */}
         <div className={style.inputContainer}>
           <form onSubmit={submit}>
             <input
-              spellCheck="false"
+              spellCheck='false'
               onChange={change}
-              type="text"
-              placeholder="search summoner..."
+              type='text'
+              placeholder='search summoner...'
             />
           </form>
           <AiOutlineSearch className={style.searchIcon} />
@@ -25,7 +38,7 @@ function Home({ change, submit, inputResponse }) {
         <h2>{inputResponse}</h2>
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
