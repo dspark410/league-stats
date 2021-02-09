e4c1e6f645b9bf81d0f1d9eed5037712876a2c62
Wesley <wesaboo@gmail.com>
2021-02-08 17:44:10 -0800

    moves ingame pill to after summonerCard

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import style from './welcome.module.css'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import style from "./welcome.module.css";
+import axios from "axios";
 //import { motion } from "framer-motion";
-import MasteryCard from '../components/MasteryCard'
-import RankCard from '../components/RankCard'
-import UnrankedCard from '../components/UnrankedCard'
-import SummonerCard from '../components/SummonerCard'
-import MatchHistoryCard from '../components/MatchHistoryCard'
-import Live from '../components/Live'
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import MatchHistoryCardSkeleton from '../components/MatchHistoryCardSkeleton'
-import MasteryCardSkeleton from '../components/MasteryCardSkeleton'
+import MasteryCard from "../components/MasteryCard";
+import RankCard from "../components/RankCard";
+import UnrankedCard from "../components/UnrankedCard";
+import SummonerCard from "../components/SummonerCard";
+import MatchHistoryCard from "../components/MatchHistoryCard";
+import Live from "../components/Live";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import MatchHistoryCardSkeleton from "../components/MatchHistoryCardSkeleton";
+import MasteryCardSkeleton from "../components/MasteryCardSkeleton";
 
 function Welcome({
   summonerInfo,
@@ -21,88 +21,88 @@ function Welcome({
   redirect,
   showNav,
 }) {
-  const [mastery, setMastery] = useState([])
-  const [rank, setRank] = useState([])
-  const [liveRank, setLiveRank] = useState([])
-  const [filteredChamps, setFilteredChamps] = useState([])
-  const [session, setSession] = useState({})
-  const [playerMatches, setPlayerMatches] = useState([])
-  const [display, setDisplay] = useState('overview')
-  const [live, setLive] = useState()
-  const [time, setTime] = useState()
-  const [loading, setLoading] = useState(true)
+  const [mastery, setMastery] = useState([]);
+  const [rank, setRank] = useState([]);
+  const [liveRank, setLiveRank] = useState([]);
+  const [filteredChamps, setFilteredChamps] = useState([]);
+  const [session, setSession] = useState({});
+  const [playerMatches, setPlayerMatches] = useState([]);
+  const [display, setDisplay] = useState("overview");
+  const [live, setLive] = useState();
+  const [time, setTime] = useState();
+  const [loading, setLoading] = useState(true);
 
-  const url = process.env.REACT_APP_API_URL || ''
+  const url = process.env.REACT_APP_API_URL || "";
 
   // Function for masteries call specific to summoner id
-  const getMasteries = (id) => axios.get(`${url}/masteries/${id}`)
+  const getMasteries = (id) => axios.get(`${url}/masteries/${id}`);
 
   // Function for rank call specific to summoner id
-  const getRank = (id) => axios.get(`${url}/rank/${id}`)
+  const getRank = (id) => axios.get(`${url}/rank/${id}`);
 
   // Function for getting match list specific to the summoner
-  const getMatchList = (id) => axios.get(`${url}/matchList/${id}`)
+  const getMatchList = (id) => axios.get(`${url}/matchList/${id}`);
 
   useEffect(() => {
     // Show nav on the welcome screen
-    showNav()
-    setLive()
+    showNav();
+    setLive();
     if (!summonerInfo.id) {
       // Checks if summonerInfo.id is available, if not grab identical copy from sessionStorage
-      const sessionData = JSON.parse(sessionStorage.getItem('summonerInfo'))
-      setSession(sessionData)
+      const sessionData = JSON.parse(sessionStorage.getItem("summonerInfo"));
+      setSession(sessionData);
 
       // Get masteries using sessionStorage and set into state
 
       getMasteries(sessionData.id).then((res) => {
-        setMastery(res.data)
-        getRank(sessionData.id).then((res) => setRank(res.data))
+        setMastery(res.data);
+        getRank(sessionData.id).then((res) => setRank(res.data));
 
         getMatchList(sessionData.accountId).then((res) =>
           setPlayerMatches(res.data.matches)
-        )
-      })
+        );
+      });
 
       // Get live game data for summoner
       axios.get(`${url}/live/${sessionData.id}`).then((res) => {
-        setLive(res.data)
+        setLive(res.data);
         // setLength(res.data.gameLength)
-      })
+      });
     } else {
       // Get masteries from state and set into state
-      setLoading(true)
+      setLoading(true);
 
       getMasteries(summonerInfo.id).then((res) => {
-        setMastery(res.data)
-        getRank(summonerInfo.id).then((res) => setRank(res.data))
+        setMastery(res.data);
+        getRank(summonerInfo.id).then((res) => setRank(res.data));
         getMatchList(summonerInfo.accountId).then((res) =>
           setPlayerMatches(res.data.matches)
-        )
-      })
+        );
+      });
 
       // Get live game data for summoner
       axios.get(`${url}/live/${summonerInfo.id}`).then((res) => {
-        setLive(res.data)
-      })
+        setLive(res.data);
+      });
       if (playerMatches.length >= 0 && mastery.length >= 0 && summonerInfo) {
         setTimeout(() => {
-          setLoading(false)
-        }, 3000)
+          setLoading(false);
+        }, 3000);
       }
     }
 
-    redirect()
+    redirect();
     // Dependency, rerenders when summonerInfo.id is ready
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [summonerInfo])
+  }, [summonerInfo]);
 
   useEffect(() => {
-    let time
-    if (live && typeof live.gameLength === 'number') {
-      setTime(live.gameLength < 0 ? live.gameLength * -1 : live.gameLength)
+    let time;
+    if (live && typeof live.gameLength === "number") {
+      setTime(live.gameLength < 0 ? live.gameLength * -1 : live.gameLength);
       time = setInterval(() => {
-        setTime((seconds) => seconds + 1)
-      }, 1000)
+        setTime((seconds) => seconds + 1);
+      }, 1000);
 
       // const liveRankArray = []
       // live.participants.forEach(async (player) => {
@@ -116,22 +116,22 @@ function Welcome({
       // })
     }
     return () => {
-      clearTimeout(time)
-    }
-  }, [live])
+      clearTimeout(time);
+    };
+  }, [live]);
 
   useEffect(() => {
     // Array to store newly created object that matches champion key to mastery key
-    const champObject = []
+    const champObject = [];
     // Nested for loop that compares mastery array to champInfo array for matches
     mastery.forEach((champ) => {
       champInfo.forEach((champion) => {
         if (champ.championId === +champion.key) {
-          const name = champion.name
-          const key = champ.championId
-          const image = champion.image.full
-          const level = champ.championLevel
-          const points = champ.championPoints
+          const name = champion.name;
+          const key = champ.championId;
+          const image = champion.image.full;
+          const level = champ.championLevel;
+          const points = champ.championPoints;
 
           // Create our own object containing neccessary data to push to champObject
           const object = {
@@ -140,114 +140,97 @@ function Welcome({
             image,
             level,
             points,
-          }
+          };
           // Push object to champObject
-          champObject.push(object)
+          champObject.push(object);
         }
-      })
-    })
+      });
+    });
     // Stores new array of object into state to be mapped
-    setFilteredChamps(champObject)
-  }, [mastery, champInfo])
+    setFilteredChamps(champObject);
+  }, [mastery, champInfo]);
   return (
-    <SkeletonTheme duration={3} color='#7a6b83' highlightColor='#e2c0f7'>
+    <SkeletonTheme duration={3} color="#7a6b83" highlightColor="#e2c0f7">
       <div className={style.rowContainer}>
         <div className={style.row1}>
-          <div className={style.summonerNameContainer}>
-            {/* <h1 className={style.summonerName}>
-              {!loading ? (
-                summonerInfo.name || session.name
-              ) : (
-                <Skeleton
-                  height={50}
-                  width={400}
-                  style={{ marginBottom: '15px' }}
-                />
-              )}
-            </h1> */}
-
-            {live ? (
-              !loading ? (
+          <div className={style.emblemContainer}>
+            {!loading ? (
+              <div className={style.nameLive}>
+                <SummonerCard version={version} summonerInfo={summonerInfo} />
                 <div className={style.inGame}>
                   <div className={style.circlePulse} />
                   In Game
                 </div>
-              ) : (
+              </div>
+            ) : (
+              <div className={style.nameLive}>
+                <Skeleton circle={true} width={125} height={125} />
                 <Skeleton
-                  style={{ border: 'none' }}
+                  style={{ border: "none" }}
                   className={`${style.inGame} `}
                 />
-              )
-            ) : (
-              ''
-            )}
-          </div>
-          <div className={style.emblemContainer}>
-            {!loading ? (
-              <SummonerCard version={version} summonerInfo={summonerInfo} />
-            ) : (
-              <Skeleton circle={true} width={125} height={125} />
+              </div>
             )}
             <div className={style.rankCardContainer}>
               {!loading ? (
                 <div className={style.rankContainer}>
                   {!rank.length ||
                   (rank.length === 1 &&
-                    rank[0].queueType === 'RANKED_FLEX_SR') ? (
-                    <UnrankedCard queue='Solo' />
+                    rank[0].queueType === "RANKED_FLEX_SR") ? (
+                    <UnrankedCard queue="Solo" />
                   ) : (
                     rank.map((ranking, i) => {
-                      return ranking.queueType === 'RANKED_SOLO_5x5' ? (
-                        <RankCard rank={ranking} queue='Solo' />
+                      return ranking.queueType === "RANKED_SOLO_5x5" ? (
+                        <RankCard rank={ranking} queue="Solo" />
                       ) : (
-                        ranking.queueType === 'RANKED_FLEX_SR' && ''
-                      )
+                        ranking.queueType === "RANKED_FLEX_SR" && ""
+                      );
                     })
                   )}
 
                   <img
-                    alt='Unranked'
+                    alt="Unranked"
                     className={style.rectangle}
                     src={process.env.PUBLIC_URL + `/images/icons/rectangle.png`}
                   />
 
                   {!rank.length ||
                   (rank.length === 1 &&
-                    rank[0].queueType === 'RANKED_SOLO_5x5') ? (
-                    <UnrankedCard queue='Flex' />
+                    rank[0].queueType === "RANKED_SOLO_5x5") ? (
+                    <UnrankedCard queue="Flex" />
                   ) : (
                     rank.map((ranking, i) => {
-                      return ranking.queueType === 'RANKED_FLEX_SR' ? (
-                        <RankCard rank={ranking} queue='Flex' />
+                      return ranking.queueType === "RANKED_FLEX_SR" ? (
+                        <RankCard rank={ranking} queue="Flex" />
                       ) : (
-                        ranking.queueType === 'RANKED_SOLO_5x5' && ''
-                      )
+                        ranking.queueType === "RANKED_SOLO_5x5" && ""
+                      );
                     })
                   )}
                 </div>
               ) : (
-                <div style={{ marginLeft: '10px' }}>
+                <div style={{ marginLeft: "10px" }}>
                   <div>
                     <Skeleton
-                      style={{ marginLeft: '10px' }}
+                      style={{ marginLeft: "10px" }}
                       circle={true}
                       width={50}
                       height={50}
                     />
                     <Skeleton
-                      style={{ marginLeft: '10px' }}
+                      style={{ marginLeft: "10px" }}
                       width={200}
                       height={48}
                     />
                   </div>
                   <div>
                     <Skeleton
-                      style={{ marginLeft: '10px' }}
+                      style={{ marginLeft: "10px" }}
                       width={125}
                       height={41}
                     />
                     <Skeleton
-                      style={{ marginLeft: '10px' }}
+                      style={{ marginLeft: "10px" }}
                       width={115}
                       height={41}
                     />
@@ -261,36 +244,36 @@ function Welcome({
         <div className={style.row2}>
           <div className={style.linksContainer}>
             <span
-              onClick={() => setDisplay('overview')}
-              to='#'
+              onClick={() => setDisplay("overview")}
+              to="#"
               className={
-                !loading && display === 'overview'
+                !loading && display === "overview"
                   ? style.underline
                   : style.live
               }
             >
               {!loading ? (
-                'Overview'
+                "Overview"
               ) : (
                 <Skeleton
-                  style={{ display: 'inlineBlock' }}
+                  style={{ display: "inlineBlock" }}
                   height={30}
                   width={74}
                 />
               )}
             </span>
             <span
-              onClick={() => setDisplay('live')}
-              to='/live'
+              onClick={() => setDisplay("live")}
+              to="/live"
               className={
-                !loading && display === 'live' ? style.underline : style.live
+                !loading && display === "live" ? style.underline : style.live
               }
             >
               {!loading ? (
-                'Live Game'
+                "Live Game"
               ) : (
                 <Skeleton
-                  style={{ display: 'inlineBlock' }}
+                  style={{ display: "inlineBlock" }}
                   height={30}
                   width={84}
                 />
@@ -299,7 +282,7 @@ function Welcome({
           </div>
         </div>
         <div className={style.row3}>
-          {display === 'overview' && playerMatches.length === 0 && !loading ? (
+          {display === "overview" && playerMatches.length === 0 && !loading ? (
             <>
               <div className={style.noMatchContainer}>
                 <div className={style.matchHeader}>Match History</div>
@@ -309,8 +292,8 @@ function Welcome({
               <div className={style.masteryCard}>
                 <div className={style.header}>
                   <img
-                    alt='mastery icon'
-                    src={process.env.PUBLIC_URL + '/images/icons/mastery.png'}
+                    alt="mastery icon"
+                    src={process.env.PUBLIC_URL + "/images/icons/mastery.png"}
                   />
                   CHAMPION MASTERY
                 </div>
@@ -323,7 +306,7 @@ function Welcome({
                   <div className={style.noChamps}>No Champions Found.</div>
                 )}
               </div>
-              {live === undefined && display === 'live' && (
+              {live === undefined && display === "live" && (
                 <div className={style.notInGame}>Summoner Is Not In Game.</div>
               )}
             </>
@@ -331,7 +314,7 @@ function Welcome({
             <>
               <div
                 className={
-                  loading && display === 'overview' ? style.row3 : style.none
+                  loading && display === "overview" ? style.row3 : style.none
                 }
               >
                 <MatchHistoryCardSkeleton />
@@ -339,7 +322,7 @@ function Welcome({
               </div>
               <div
                 className={
-                  !loading && display === 'overview' ? style.row3 : style.none
+                  !loading && display === "overview" ? style.row3 : style.none
                 }
               >
                 <MatchHistoryCard
@@ -360,7 +343,7 @@ function Welcome({
               </div>
               <div
                 className={
-                  live === undefined && display === 'live'
+                  live === undefined && display === "live"
                     ? style.notInGame
                     : style.none
                 }
@@ -369,7 +352,7 @@ function Welcome({
               </div>
               <div
                 className={
-                  live && display === 'live' ? style.liveContainer : style.none
+                  live && display === "live" ? style.liveContainer : style.none
                 }
               >
                 {live ? (
@@ -382,7 +365,7 @@ function Welcome({
                     liveRank={liveRank}
                   />
                 ) : (
-                  ''
+                  ""
                 )}
               </div>
             </>
@@ -390,7 +373,7 @@ function Welcome({
         </div>
       </div>
     </SkeletonTheme>
-  )
+  );
 }
 
-export default Welcome
+export default Welcome;
