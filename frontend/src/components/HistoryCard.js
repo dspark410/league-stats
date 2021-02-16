c353719b84129a872303d15d56f41ddae834391b
mistahpig <dspark410@gmail.com>
2021-02-16 13:15:30 -0800

    adds react transition group and changes championdetail css for summoner name skins

--- frontend/src/components/HistoryCard.js
+++ frontend/src/components/HistoryCard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import style from './historycard.module.css'
 import HistoryCardComplex from './HistoryCardComplex'
 import HistoryCardSimple from './HistoryCardSimple'
+import { CSSTransition } from 'react-transition-group'
 
 function HistoryCard({ game, spells, runes, summonerInfo, getPlayerName }) {
   const [open, setOpen] = useState(false)
@@ -15,20 +16,33 @@ function HistoryCard({ game, spells, runes, summonerInfo, getPlayerName }) {
   }
 
   return (
-    <div className={`${style.fadeIn}`}>
-      {!open ? (
-        <HistoryCardSimple game={game} clickArrow={clickArrow} />
-      ) : (
-        <HistoryCardComplex
-          game={game}
-          spells={spells}
-          runes={runes}
-          summonerInfo={summonerInfo}
-          getPlayerName={getPlayerName}
-          clickArrow={clickArrow}
-        />
-      )}
-    </div>
+    <CSSTransition
+      in={open}
+      timeout={300}
+      classNames={{
+        enter: style.enter,
+        enterActive: style.enterActive,
+        enterDone: style.enterDone,
+        exit: style.exit,
+        exitActive: style.exitActive,
+        exitDone: style.exitDone,
+      }}
+    >
+      <div className={`${style.fadeIn}`}>
+        {!open ? (
+          <HistoryCardSimple game={game} clickArrow={clickArrow} />
+        ) : (
+          <HistoryCardComplex
+            game={game}
+            spells={spells}
+            runes={runes}
+            summonerInfo={summonerInfo}
+            getPlayerName={getPlayerName}
+            clickArrow={clickArrow}
+          />
+        )}
+      </div>
+    </CSSTransition>
   )
 }
 
