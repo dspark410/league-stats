57b2094cdc3b69c72a2303cbc23c70ffd904d818
Wesley <wesaboo@gmail.com>
2021-02-25 17:22:11 -0800

    adds inputref to home screen, fixed navbar issue with not being able to select summoner of same name in different region

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Link } from 'react-router-dom'
-import style from './navbar.module.css'
-import { AiOutlineSearch, AiOutlineInfoCircle } from 'react-icons/ai'
-import { IoSearchCircle } from 'react-icons/io5'
-import { regions } from '../utils/constant'
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import style from "./navbar.module.css";
+import { AiOutlineSearch, AiOutlineInfoCircle } from "react-icons/ai";
+import { IoSearchCircle } from "react-icons/io5";
+import { regions } from "../utils/constant";
 
 function Navbar({
   visibility,
@@ -23,19 +23,19 @@ function Navbar({
   summonerInfo,
   inputRef,
 }) {
-  const [vis, setVis] = useState(visibility)
+  const [vis, setVis] = useState(visibility);
 
-  const inputEl = useRef(false)
+  const inputEl = useRef(false);
 
   useEffect(() => {
-    setVis(visibility)
-  }, [visibility])
+    setVis(visibility);
+  }, [visibility]);
 
   return (
     <>
-      <nav className={style.navbar} style={{ display: !vis ? 'none' : 'flex' }}>
+      <nav className={style.navbar} style={{ display: !vis ? "none" : "flex" }}>
         <div className={style.navHeader}>
-          <Link to='/' className={style.navbarLogo}>
+          <Link to="/" className={style.navbarLogo}>
             League Stats
           </Link>
         </div>
@@ -57,10 +57,10 @@ function Navbar({
                 </select>
                 <input
                   className={style.input}
-                  spellCheck='false'
+                  spellCheck="false"
                   onChange={change}
-                  type='text'
-                  placeholder='search summoner...'
+                  type="text"
+                  placeholder="search summoner..."
                   onFocus={handleFocus}
                   onBlur={handleBlur}
                   value={inputValue}
@@ -95,9 +95,9 @@ function Navbar({
                     <div
                       // onClick={skeletonTrue}
                       onMouseDown={submit}
-                      value='mistahpig'
-                      region='NA1'
-                      icon='7'
+                      value="mistahpig"
+                      region="NA1"
+                      icon="7"
                       className={style.storageSummoner}
                     >
                       <div className={style.regionContainer}>
@@ -105,16 +105,16 @@ function Navbar({
                       </div>
 
                       <img
-                        alt='profile icon'
+                        alt="profile icon"
                         className={style.profileIcon}
                         // Grab profile icon
                         src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/7.png`}
                       />
                       <span
                         // onMouseDown={submit}
-                        value='mistahpig'
-                        region='NA1'
-                        icon='7'
+                        value="mistahpig"
+                        region="NA1"
+                        icon="7"
                         className={style.summoner}
                       >
                         mistahpig
@@ -130,25 +130,25 @@ function Navbar({
                     <div
                       // onClick={skeletonTrue}
                       onMouseDown={submit}
-                      value='Dambitwes'
-                      region='NA1'
-                      icon='3466'
+                      value="Dambitwes"
+                      region="NA1"
+                      icon="3466"
                       className={style.storageSummoner}
                     >
                       <div className={style.regionContainer}>
                         <span className={style.region}>NA</span>
                       </div>
                       <img
-                        alt='profile icon'
+                        alt="profile icon"
                         className={style.profileIcon}
                         // Grab profile icon
                         src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/3466.png`}
                       />
                       <span
                         // onMouseDown={submit}
-                        value='Dambitwes'
-                        region='NA1'
-                        icon='3466'
+                        value="Dambitwes"
+                        region="NA1"
+                        icon="3466"
                         className={style.summoner}
                       >
                         Dambitwes
@@ -174,7 +174,8 @@ function Navbar({
                         className={style.topLayer}
                         onMouseDown={
                           summoner[0].toLowerCase() ===
-                          summonerInfo.name.toLowerCase()
+                            summonerInfo.name.toLowerCase() &&
+                          summoner[1] === region
                             ? handleBlur
                             : submit
                         }
@@ -187,7 +188,7 @@ function Navbar({
                       </div>
 
                       <img
-                        alt='profile icon'
+                        alt="profile icon"
                         className={style.profileIcon}
                         // Grab profile icon
                         src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${summoner[2]}.png`}
@@ -221,24 +222,24 @@ function Navbar({
 
         <ul className={style.navMenu}>
           <li className={style.navItem}>
-            <Link to='/' className={style.navLinks}>
+            <Link to="/" className={style.navLinks}>
               Home
             </Link>
           </li>
           <li className={style.navItem}>
-            <Link to='/champions' className={style.navLinks}>
+            <Link to="/champions" className={style.navLinks}>
               Champions
             </Link>
           </li>
           <li className={style.navItem}>
-            <Link to='/leaderboard' className={style.navLinks}>
+            <Link to="/leaderboard" className={style.navLinks}>
               Leaderboard
             </Link>
           </li>
         </ul>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
