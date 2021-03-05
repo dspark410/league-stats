60cb622510cd0b3b981c9510315cc40f3e528992
Wesley <wesaboo@gmail.com>
2021-03-05 14:25:48 -0800

    cleaned up some error, extended skeleton time, remove source tokens

--- frontend/src/pages/LeaderboardSkeleton.js
+++ frontend/src/pages/LeaderboardSkeleton.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import style from './leaderboard.module.css'
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import React from "react";
+import style from "./leaderboard.module.css";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 function LeaderboardSkeleton({ loading }) {
   return (
-    <SkeletonTheme duration={3} color='#7a6b83' highlightColor='#e2c0f7'>
+    <SkeletonTheme duration={3} color="#7a6b83" highlightColor="#e2c0f7">
       <div
-        style={loading ? { display: 'block' } : { display: 'none' }}
+        style={loading ? { display: "block" } : { display: "none" }}
         className={style.leaderboardContainer}
       >
         <h1 className={style.leaderHeader}> Ranked Leaderboard</h1>
@@ -14,37 +14,32 @@ function LeaderboardSkeleton({ loading }) {
           <Skeleton
             width={163}
             height={41}
-            style={{ margin: '10px 10px 15px 10px' }}
+            style={{ margin: "10px 10px 15px 10px" }}
           />
           <Skeleton
             width={58}
             height={41}
-            style={{ margin: '10px 10px 15px 10px' }}
+            style={{ margin: "10px 10px 15px 10px" }}
           />
         </div>
         <div className={style.tableContainer}>
-          {Array.from({ length: 26 }).map((num) => (
-            <Skeleton
-              key={num}
-              className={style.table}
-              width={945}
-              height={49}
-            />
+          {Array.from({ length: 26 }).map((num, i) => (
+            <Skeleton key={i} className={style.table} width={945} height={49} />
           ))}
         </div>
         <div className={style.buttonContainer}>
-          {Array.from({ length: 13 }).map((num) => (
+          {Array.from({ length: 13 }).map((num, i) => (
             <Skeleton
-              key={num}
+              key={i}
               width={50}
               height={31}
-              style={{ margin: '10px 5px 15px 5px' }}
+              style={{ margin: "10px 5px 15px 5px" }}
             />
           ))}
         </div>
       </div>
     </SkeletonTheme>
-  )
+  );
 }
 
-export default LeaderboardSkeleton
+export default LeaderboardSkeleton;
