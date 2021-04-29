245a3d7cea8b21e445c26468df2553309123d62a
David Park <dspark410@gmail.com>
2021-04-29 15:37:41 -0700

    Revert "Redux backup"

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,36 +1,18 @@
-/** @format */
-
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import {
-  summonerInfoReducer,
-  moreMatchesReducer,
-  dependencyReducer,
-  inputReducer,
-} from './reducers'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { summonerInfoReducer, moreMatchesReducer } from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducers = combineReducers({
-  dependency: dependencyReducer,
-  input: inputReducer,
   summoner: summonerInfoReducer,
   getMoreMatches: moreMatchesReducer,
-})
+});
 
-// const initialState = {
-//   input: {
-//     showPrevSearches: false,
-//     hideAnimation: true,
-//     prevSearches: ['mistahpig'],
-//   },
-// }
-
-const middleware = [thunk]
+const middleware = [thunk];
 
 const store = createStore(
   reducers,
-  // initialState,
   composeWithDevTools(applyMiddleware(...middleware))
-)
+);
 
-export default store
+export default store;
