e909c8b24a799bfb802e5c81b0daf602db071a6d
Wesley <wesaboo@gmail.com>
2021-05-01 14:47:13 -0700

    fix remove summoner

--- frontend/src/redux/reducers.js
+++ frontend/src/redux/reducers.js
@@ -11,59 +11,59 @@ import {
   ANIMATE_SHOW,
   ANIMATE_HIDE,
   REMOVE_SUMMONER,
-} from './constants'
+} from "./constants";
 
 const prevSearchesLocal =
-  JSON.parse(localStorage.getItem('searchedSummoner')) || []
+  JSON.parse(localStorage.getItem("searchedSummoner")) || [];
 
-const dependencyInitial = {}
+const dependencyInitial = {};
 
 const inputInitial = {
   showPrevSearches: false,
   hideAnimation: true,
   prevSearches: prevSearchesLocal,
-}
+};
 
 const summonerInfoInitial = {
   summLoading: false,
   data: {},
-  error: '',
-}
+  error: "",
+};
 
-const moreMatchesInitial = {}
+const moreMatchesInitial = {};
 
 export const dependencyReducer = (state = dependencyInitial, action) => {
   switch (action.type) {
     case GET_DEPENDENCY:
-      return { ...state, ...action.payload }
+      return { ...state, ...action.payload };
     case ERROR:
-      return { ...state, error: action.payload }
+      return { ...state, error: action.payload };
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const summonerInfoReducer = (state = summonerInfoInitial, action) => {
   switch (action.type) {
     case LOADING:
-      return { ...state, summLoading: true }
+      return { ...state, summLoading: true };
     case GET_SUMMONER_INFO:
-      return { ...state, summLoading: false, data: action.payload }
+      return { ...state, summLoading: false, data: action.payload };
     case ERROR:
-      return { ...state, summLoading: false, error: action.payload }
+      return { ...state, summLoading: false, error: action.payload };
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const moreMatchesReducer = (state = moreMatchesInitial, action) => {
   switch (action.type) {
     case GET_MORE_MATCHES:
-      return { ...state, ...action.payload }
+      return { ...state, ...action.payload };
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const inputReducer = (state = inputInitial, action) => {
   switch (action.type) {
@@ -71,34 +71,35 @@ export const inputReducer = (state = inputInitial, action) => {
       return {
         ...state,
         showPrevSearches: true,
-      }
+      };
     case HIDE_STORAGE:
       return {
         ...state,
         showPrevSearches: false,
-      }
+      };
     case ANIMATE_SHOW:
       return {
         ...state,
         hideAnimation: true,
-      }
+      };
     case ANIMATE_HIDE:
       return {
         ...state,
         hideAnimation: false,
-      }
+      };
     case REMOVE_SUMMONER:
       return {
         ...state,
         prevSearches: state.prevSearches.filter((summoner) => {
+          console.log("summoner", summoner[0], summoner[1]);
+          console.log("action", action.payload[0], action.payload[1]);
           return (
-            summoner[0] &&
-            summoner[1] !== action.payload[0] &&
-            action.payload[1]
-          )
+            summoner[0] !== action.payload[0] ||
+            summoner[1] !== action.payload[1]
+          );
         }),
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};
